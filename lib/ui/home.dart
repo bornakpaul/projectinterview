d77e46ac1601c8b4584da68792b77515522a80c5
Bornak Paul <bornakpaul@gmail.com>
2022-04-05 08:19:13 +0530

    Changes completed

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -20,18 +20,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-//   bool subSelected = false;
-//   void _manageStateForChildWidget(bool newValue) {
-//     setState(() {
-//       subSelected = newValue;
-//     });
-//   }
-
   List<SubDetails> subdetails = [
     SubDetails(subscriptionName: 'Yearly', price: '10', discount: '20'),
     SubDetails(subscriptionName: 'Monthly', price: '50', discount: '10'),
     SubDetails(subscriptionName: 'Weekly', price: '20', discount: '3'),
     SubDetails(subscriptionName: 'Half-yearly', price: '100', discount: '20'),
+    SubDetails(subscriptionName: 'One-Time', price: '20', discount: '10'),
   ];
   String? _subscriptionSelected;
 
@@ -67,15 +61,15 @@ class _HomeScreenState extends State<HomeScreen> {
                     crossAxisSpacing: 10,
                     crossAxisCount: 2),
                 itemBuilder: (context, index) {
+                  // if (subdetails[index].subscriptionName == "One-Time") {
+                  //   subdetails.removeAt(index);
+                  // }
                   final subscriptionSelected =
                       subdetails[index].subscriptionName;
                   final isSelected =
                       subscriptionSelected == _subscriptionSelected;
                   return SubscriptionTile(
                     detailsList: subdetails,
-                    price: subdetails[index].price,
-                    discount: subdetails[index].discount,
-                    subscription: subdetails[index].subscriptionName,
                     isActive: isSelected,
                     notifyParent: (bool selected) {
                       setState(() {
@@ -91,11 +85,21 @@ class _HomeScreenState extends State<HomeScreen> {
               const SizedBox(
                 height: 30.0,
               ),
-              //   OneTimeButton(
-              //     subscriptionPack: SubscriptionPack.oneTime,
-              //     isActive: subSelected,
-              //     notifyParent: _manageStateForChildWidget,
-              //   ),
+              // ListView.builder(
+              //   shrinkWrap: true,
+              //   itemBuilder: (context, index) {
+              //     final subscriptionSelected =
+              //         subdetails[index].subscriptionName;
+              //     final isSelected =
+              //         subscriptionSelected == _subscriptionSelected;
+              //     return OneTimeButton(
+              //       detailsList: subdetails,
+              //       isActive: isSelected,
+              //       index: index,
+              //     );
+              //   },
+              //   itemCount: subdetails.length,
+              // ),
               const SizedBox(
                 height: 30.0,
               ),
@@ -125,9 +129,6 @@ enum SubscriptionPack { weekly, monthly, quarterly, yearly, oneTime, none }
 
 class SubscriptionTile extends StatelessWidget {
   final List<SubDetails> detailsList;
-  final String price;
-  final String discount;
-  final String subscription;
   final ValueChanged<bool>? notifyParent;
   final bool isActive;
   final int index;
@@ -135,9 +136,6 @@ class SubscriptionTile extends StatelessWidget {
   const SubscriptionTile({
     Key? key,
     required this.detailsList,
-    required this.price,
-    required this.discount,
-    required this.subscription,
     this.notifyParent,
     required this.isActive,
     required this.index,
@@ -190,21 +188,21 @@ class SubscriptionTile extends StatelessWidget {
                     color: Colors.deepOrangeAccent,
                   ),
                   child: Text(
-                    'SAVE ' + discount + '%',
+                    'SAVE ' + detailsList[index].discount + '%',
                     style: TextStyle(color: Colors.white, fontSize: 12),
                   ),
                 )
               ],
             ),
             Text(
-              'Rs.' + price,
+              'Rs.' + detailsList[index].price,
               style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.w500,
               ),
             ),
             Text(
-              subscription,
+              detailsList[index].subscriptionName,
               style: const TextStyle(
                 fontSize: 14,
                 fontWeight: FontWeight.w600,
@@ -224,27 +222,25 @@ class SubscriptionTile extends StatelessWidget {
 }
 
 class OneTimeButton extends StatelessWidget {
-  final SubscriptionPack subscriptionPack;
+  final List<SubDetails> detailsList;
   final ValueChanged<bool>? notifyParent;
   final bool isActive;
+  final int index;
 
   OneTimeButton({
     Key? key,
-    required this.subscriptionPack,
+    required this.detailsList,
     this.notifyParent,
-    this.isActive = false,
+    required this.isActive,
+    required this.index,
   }) : super(key: key);
-
-  void manageState() {
-    notifyParent!(!isActive);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Visibility(
-      visible: subscriptionPack == SubscriptionPack.oneTime,
+      visible: true,
       child: GestureDetector(
-        onTap: manageState,
+        onTap: () {},
         child: Column(
           children: [
             Row(
@@ -275,7 +271,7 @@ class OneTimeButton extends StatelessWidget {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(8),
                 border: Border.all(
-                    color: isActive ? Color(0xff05837F) : Colors.transparent),
+                    color: false ? Color(0xff05837F) : Colors.transparent),
                 boxShadow: const [
                   BoxShadow(
                     color: Color.fromRGBO(112, 144, 176, 0.08),
@@ -301,8 +297,7 @@ class OneTimeButton extends StatelessWidget {
                       width: 10,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(50),
-                        color:
-                            isActive ? Color(0xff05837F) : Colors.transparent,
+                        color: false ? Color(0xff05837F) : Colors.transparent,
                       ),
                     ),
                   ),
@@ -312,9 +307,9 @@ class OneTimeButton extends StatelessWidget {
                   Expanded(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text('One time'),
-                        Text('₹200'),
+                      children: [
+                        Text(detailsList[index].subscriptionName),
+                        Text(detailsList[index].price),
                       ],
                     ),
                   )
